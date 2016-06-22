7258945e06195c054e1c11a3ad374c843f030aca
hlassiege <h.lassiege@gmail.com>
2016-06-22 22:35:01 +0200

    missing files

---
+++ java/src/main/java/com/lateralthoughts/blog/model/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.lateralthoughts.blog.model;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+}
